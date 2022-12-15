8a1cfba0963a058de37db2463a9f706ff34e0608
Guolong Li <Guolong.LiRealtor@gmail.com>
2022-12-15 01:24:16 -0600

    MVCproject

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,13 +1,34 @@
+using System;
+
+using System.Data;
+
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySql.Data.MySqlClient;
+using Testing;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connection = builder.Configuration.GetConnectionString("bestbuy");
+//var test = "Server=localhost;Database=bestbuy;uid=root;Pwd=password;Port=3306;"; 
+builder.Services.AddScoped<IDbConnection>((s) =>
+{
+    IDbConnection conn = new MySqlConnection(connection);
+    conn.Open();
+    return conn;
+
+});
+
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
+
 
 
 // Configure the HTTP request pipeline.
@@ -20,9 +41,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
